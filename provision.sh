7f8ec6c13f556d80adabad923e95439b6732a0a1
VlN9 <tron4ik.ne@gmail.com>
2023-01-06 19:06:46 +0200

    debug

--- provision.sh
+++ provision.sh
@@ -4,9 +4,9 @@ if [ "$ACTION" == "create" ]; then
     eksctl $ACTION cluster -f cluster.yaml
     ./kubectl-action.sh
     export RECORD_ACTION="CREATE"
-    ./deploy-dns-record.sh
+    ./deploy-dns-records.sh
 else
     eksctl $ACTION cluster -f cluster.yaml
     export RECORD_ACTION="DELETE"
-    ./deploy-dns-record.sh
+    ./deploy-dns-records.sh
 fi
